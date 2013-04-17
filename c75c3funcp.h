4a6edab93c97edd29407d0170696b18f21a2f5f4
CarnationTL <tongl@bjcarnation.com.cn>
2013-04-17 16:37:22 +0800

    del pw supply

--- c75c3funcp.h
+++ c75c3funcp.h
@@ -42,7 +42,6 @@ typedef int(* FPTR_GET_REFLOSS_THOLD)(int, int, int, double*);
 typedef int(* FPTR_SET_SIGLOSS_THOLD)(int, int, int, double);
 typedef int(* FPTR_GET_SIGLOSS_THOLD)(int, int, int, double*);
 
-//typedef int(* FPTR_SET_PW_SUPPLY)(int, int, int, unsigned short);
 typedef int(* FPTR_SET_REF_VOL)(int, int, double);     //set card ref not model..
 typedef int(* FPTR_SET_REF_FEQ)(int, int, unsigned int);
 
